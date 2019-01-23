a4fa585337fa0b15e39435da5006c60ef4f51389
FabianMoretti <dp120793tpi@gmail.com>
2019-01-23 15:16:20 +0200

    covered with unit-tests

--- src/app/core/services/cookie.service.spec.ts
+++ src/app/core/services/cookie.service.spec.ts
@@ -3,10 +3,23 @@ import { TestBed } from '@angular/core/testing';
 import { CookieService } from './cookie.service';
 
 describe('CookieService', () => {
+
+  const mockCookieName = 'name';
+  const mockCookieValue = 'value';
+  let cookie;
+
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     const service: CookieService = TestBed.get(CookieService);
     expect(service).toBeTruthy();
   });
+
+  it('get method should return correct value', () => {
+    const cookieService: CookieService = TestBed.get(CookieService);
+    cookieService.set(mockCookieName, mockCookieValue);
+    cookie = cookieService.get(mockCookieName);
+    expect(cookie).toBe('value');
+  });
+
 });
