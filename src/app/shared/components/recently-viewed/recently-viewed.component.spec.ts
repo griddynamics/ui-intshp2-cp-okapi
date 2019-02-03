5b0b0f923d262c57a636322ad0881bf494434f4e
unknown <nataliia.fa;inska@gmail.com>
2019-02-03 23:46:22 +0200

    fix ng test

--- src/app/shared/components/recently-viewed/recently-viewed.component.spec.ts
+++ src/app/shared/components/recently-viewed/recently-viewed.component.spec.ts
@@ -1,10 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { RecentlyViewedComponent } from './recently-viewed.component';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { ProductItemShortComponent } from '../product-item/product-item-short/product-item-short.component';
 import { CarouselItemComponent } from '../carousel/carousel-item/carousel-item.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { HeadingComponent } from '../heading/heading.component';
 
 describe('RecentlyViewedComponent', () => {
   let component: RecentlyViewedComponent;
@@ -16,7 +17,8 @@ describe('RecentlyViewedComponent', () => {
       declarations: [ RecentlyViewedComponent,
                       CarouselComponent,
                       ProductItemShortComponent,
-                      CarouselItemComponent],
+                      CarouselItemComponent,
+                      HeadingComponent],
     })
     .compileComponents();
   }));
