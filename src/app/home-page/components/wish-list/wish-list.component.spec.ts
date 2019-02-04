94564ff45dc9dd65bbe4a76702008c9eb3842431
taras1313 <jakimuk.taras.semenowuch@gmail.com>
2019-02-04 14:48:14 +0200

    moved heading to wish list + minor fix

--- src/app/home-page/components/wish-list/wish-list.component.spec.ts
+++ src/app/home-page/components/wish-list/wish-list.component.spec.ts
@@ -5,6 +5,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ProductItemShortComponent } from 'src/app/shared/components/product-item/product-item-short/product-item-short.component';
 import { LoadMoreComponent } from 'src/app/shared/components/load-more/load-more.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeadingComponent } from 'src/app/shared/components/heading/heading.component';
 
 describe('WishlistComponent', () => {
   let component: WishListComponent;
@@ -12,7 +13,7 @@ describe('WishlistComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [WishListComponent, ProductItemShortComponent, LoadMoreComponent],
+      declarations: [WishListComponent, ProductItemShortComponent, LoadMoreComponent, HeadingComponent],
       imports: [ RouterTestingModule, HttpClientModule ]
     })
       .compileComponents();
