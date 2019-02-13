57e11f3261ad1ec50b889de02e3760d4feff3949
dnistrova <natalka.dnistrova@gmail.com>
2019-02-13 14:54:52 +0200

    price range

--- src/app/product-list-page/components/product-list-page/filters/filters.component.ts
+++ src/app/product-list-page/components/product-list-page/filters/filters.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.scss']
 })
-export class FiltersComponent implements OnInit {
+export class FiltersComponent {
 
-  constructor() { }
+  public isChecked = false;
+  public rangeValue = 0;
+  public range = [10, 100];
 
-  ngOnInit() {
+  toggleCheck(event) {
+    event.target.checked ? this.isChecked = true : this.isChecked = false;
+    console.log(this.isChecked);
+  }
+  valueChanged(e) {
+    this.rangeValue = e.target.value;
+
+    if (this.range[0] > this.range[1]) {
+      this.range.reverse();
+    }
   }
 
+  validateMin(e) {
+    if (e.target.value >= this.range[1]) {
+      e.preventDefault();
+      this.range[0] = this.range[1];
+    }
+  }
+
+  validateMax(e) {
+    if (e.target.value <= this.range[0]) {
+      e.preventDefault();
+      this.range[1] = this.range[0];
+    }
+  }
 }
