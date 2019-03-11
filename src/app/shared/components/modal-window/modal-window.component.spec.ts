8050dcc5ffcbfd7ffe29b95ce1bac065964b5856
NFalinska <nataliia.falinska@gmail.com>
2019-03-11 17:28:29 +0200

    fixed bugs for popup

--- src/app/shared/components/modal-window/modal-window.component.spec.ts
+++ src/app/shared/components/modal-window/modal-window.component.spec.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModalWindowComponent } from './modal-window.component';
@@ -8,7 +9,8 @@ describe('ModalWindowComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ModalWindowComponent ]
+      declarations: [ ModalWindowComponent ],
+      imports: [BrowserAnimationsModule]
     })
     .compileComponents();
   }));
