cf915a540708b74b128c141bfa846aa640117131
NFalinska <nataliia.falinska@gmail.com>
2019-03-12 16:25:30 +0200

    ng test

--- src/app/core/services/global-error-handler.service.spec.ts
+++ src/app/core/services/global-error-handler.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-
 import { GlobalErrorHandlerService } from './global-error-handler.service';
 
 describe('GlobalErrorHandlerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      GlobalErrorHandlerService
+    ]
+  }));
 
   it('should be created', () => {
     const service: GlobalErrorHandlerService = TestBed.get(GlobalErrorHandlerService);
