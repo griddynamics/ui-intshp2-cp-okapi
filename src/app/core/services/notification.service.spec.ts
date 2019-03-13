b9a05d01f2802f969ba35dfca191a697a417aeb5
dnistrova <natalka.dnistrova@gmail.com>
2019-03-13 16:04:44 +0200

    fixing tests

--- src/app/core/services/notification.service.spec.ts
+++ src/app/core/services/notification.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NotificationService } from './notification.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('NotificationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientModule, HttpClientTestingModule, MatSnackBarModule ]
+  }));
 
   it('should be created', () => {
     const service: NotificationService = TestBed.get(NotificationService);
